6beffc209540112d3abadfc4b9689da3df48ad89
laici00 <109146811+laici00@users.noreply.github.com>
2022-07-12 19:32:00 +0800

    upload file littlegame

---
+++ o.js
@@ -0,0 +1,57 @@
+function validate() {
+
+    var name = document.getElementById("name").value;
+    var text = document.getElementById("text").value;
+
+    if (name === "ywfmdilbnlybd") {
+        document.getElementById("t1").textContent = "//Count letters & add.";
+        document.getElementById("t2").textContent = "";
+        document.getElementById('name').value = null;
+    }
+
+    if (name === "57") {
+        document.getElementById("text").textContent = "//combine tres hidden numero.";
+        document.getElementById("text2").textContent = "//Top to bottom.";
+
+        var img = document.createElement("img");
+       
+        img.src = "https://i.imgur.com/mZVLypZ.jpg";
+        var src = document.getElementById("pic");
+        img.style = "width:400px;height:500px;";
+        var style = document.getElementById("pic");
+        src.appendChild(img);
+        style.appendChild(img);
+        document.getElementById('name').value = null;
+        document.getElementById("t1").textContent = null;
+        document.getElementById("t2").textContent = null;
+        
+        document.getElementById("title").style = "margin-top : 0px;";
+    }
+
+    if (name === "271691") {
+        
+        
+        document.getElementById("t1").textContent = "//Convert numero to letters. ";
+        document.getElementById("text").textContent = null;
+        document.getElementById("text2").textContent = null;
+        document.getElementById("name").value = null;
+        document.getElementById("link").textContent = "https://streamable.com/xokser";
+        document.getElementById("link").href = "https://streamable.com/xokser";
+    }
+
+    if (name === "tragedy" || name === "TRAGEDY") {
+        document.getElementById("link").textContent = null;
+        document.getElementById("link").href = null;
+        document.getElementById("t1").textContent = "//Chapter I completed.";
+        document.getElementById("link2").style = "color:yellow";
+        document.getElementById("link2").textContent = "<o>";
+        document.getElementById("link2").href = "https://upcdn.io/W142hJkJ3b6ASE2ZzDhiow6";
+    }
+
+
+
+}
+
+
+
+
---
+++ o2.js
@@ -0,0 +1,61 @@
+var counter = 0;
+
+function v1() {
+    if (counter === 0) {
+        counter++;
+    }
+}
+
+function v2() {
+    if (counter === 1) {
+        counter++;
+    }
+
+}
+
+function v3() {
+     if (counter === 2 ) {
+        counter++;
+    }
+}
+
+function v4() {
+     if (counter === 3 ) {
+        counter++;
+    }
+
+}
+
+function v5() {
+     if (counter === 4 ) {
+        counter++;
+    }
+
+}
+
+function v6() {
+     if (counter === 5 ) {
+        counter++;
+    }
+
+}
+
+function v7() {
+     if (counter === 6 ) {
+        counter++;
+    }
+
+}
+
+function v8() {
+     if (counter === 7) {
+        alert("                                                       <o>");
+        document.getElementById("coor1").textContent = null;
+        document.getElementById("coor").textContent = "//Chapter II Completed";
+        document.getElementById("yellow").textContent = "<o>";
+        document.getElementById("yellow").style = "color: yellow;";
+        document.getElementById("yellow").href = "http://www.youtube.com";
+        
+    }
+
+}
