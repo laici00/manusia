04cf2c7c5aed3b8c06e19ad548878e63a664087f
laici00 <109146811+laici00@users.noreply.github.com>
2022-07-13 22:24:25 +0800

    Update o2.js

--- o2.js
+++ o2.js
@@ -14,48 +14,49 @@ function v2() {
 }
 
 function v3() {
-     if (counter === 2 ) {
+    if (counter === 2) {
         counter++;
     }
 }
 
 function v4() {
-     if (counter === 3 ) {
+    if (counter === 3) {
         counter++;
     }
 
 }
 
 function v5() {
-     if (counter === 4 ) {
+    if (counter === 4) {
         counter++;
     }
 
 }
 
 function v6() {
-     if (counter === 5 ) {
+    if (counter === 5) {
         counter++;
     }
 
 }
 
 function v7() {
-     if (counter === 6 ) {
+    if (counter === 6) {
         counter++;
     }
 
 }
 
 function v8() {
-     if (counter === 7) {
+    if (counter === 0) {
         alert("                                                       <o>");
         document.getElementById("coor1").textContent = null;
-        document.getElementById("coor").textContent = "//Chapter II Completed.";
+        document.getElementById("coor").textContent = "//Chapter II Completed";
         document.getElementById("yellow").textContent = "<o>";
-        document.getElementById("yellow").style = "color: yellow;";
+        document.getElementById("yellow").style = "text-decoration: none !important; cursor: pointer; color: yellow;";
         document.getElementById("yellow").href = "o3.html";
-        
+        document.getElementById("coor4").textContent = "test";
+
     }
 
 }
