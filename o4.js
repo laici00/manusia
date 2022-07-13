09aa285646162784998d200be036e8bb1847d66d
laici00 <109146811+laici00@users.noreply.github.com>
2022-07-13 11:46:04 +0800

    upload o4

---
+++ o4.js
@@ -0,0 +1,47 @@
+function validate1() {
+
+    var b1 = document.getElementById("b1").value;
+    var b2 = document.getElementById("b2").value;
+    var b3 = document.getElementById("b3").value;
+    var b4 = document.getElementById("b4").value;
+    var b5 = document.getElementById("b5").value;
+    var b6 = document.getElementById("b6").value;
+    var b7 = document.getElementById("b7").value;
+    var b8 = document.getElementById("b8").value;
+    var b9 = document.getElementById("b9").value;
+    var b10 = document.getElementById("b10").value;
+
+
+    if (b1 === "2" &&
+            b2 === "8" &&
+            b3 === "15" &&
+            b4 === "21" &&
+            b5 === "29" &&
+            b6 === "34" &&
+            b7 === "39" &&
+            b8 === "47" &&
+            b9 === "53" &&
+            b10 === "60") {
+        alert("                                                       <o>");
+        document.getElementById("code").textContent = "<o>";
+        document.getElementById("code").href = "https://i.imgur.com/xnmpNDh.png";
+
+    } else {
+        document.getElementById("b1").value = null;
+        document.getElementById("b2").value = null;
+        document.getElementById("b3").value = null;
+        document.getElementById("b4").value = null;
+        document.getElementById("b5").value = null;
+        document.getElementById("b6").value = null;
+        document.getElementById("b7").value = null;
+        document.getElementById("b8").value = null;
+        document.getElementById("b9").value = null;
+        document.getElementById("b10").value = null;
+
+    }
+
+}
+
+
+
+
